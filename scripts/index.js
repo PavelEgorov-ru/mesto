d1eb693e586e37e2669733e92312bedc120aa880
PavelEgorov-ru <pkotofffff@yandex.ru>
2021-03-21 09:35:18 +0300

    исправлен скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,29 @@
-//объявили перененную попап
 let popup = document.querySelector('.popup');
 
-//объявляем переменную кнопки редактирования профиля, на клик по которой откроется попап
 let popupEdit = document.querySelector('.profile__edit-button');
 
-//объявляем переменную кнопки закрытия попап
 let popupClose = document.querySelector('.popup__close-icon');
 
 let form = document.querySelector('.popup__content');
 
 let nameProfile = document.querySelector('.profile__title');
+
 let jobProfile= document.querySelector('.profile__subtitle');
+
 let nameInput = document.querySelector ('.popup__item_el_name');
+
 let jobInput = document.querySelector ('.popup__item_el_comment');
-nameInput.textContent=nameProfile.value;
-jobInput.textContent=jobProfile.value;
+
+
+
 
 // функция открывающая попап
 function openPopup () {
   popup.classList.add('popup_visible');
-  nameInput.textContent=nameProfile.value;
-  jobInput.textContent=jobProfile.value;
+
+nameInput.value = nameProfile.textContent;
+jobInput.value = jobProfile.textContent;
+  
 }
 
 // функция закрывающая попап
@@ -32,7 +35,9 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   
   nameProfile.textContent=nameInput.value;
+  
   jobProfile.textContent=jobInput.value;
+
 
   closePopup();
 }
